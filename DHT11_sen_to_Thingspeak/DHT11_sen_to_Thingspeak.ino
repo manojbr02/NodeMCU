317ca05b83e76ea98df56688ebc1a84dc1e9e225
MANOJ BR <manojmidchi02@gmail.com>
2021-10-11 22:12:12 +0530

    DHT11 sensor readings upload to thingspeak

---
+++ DHT11_sen_to_Thingspeak/DHT11_sen_to_Thingspeak.ino
@@ -0,0 +1,48 @@
+
+#include <ESP8266WiFi.h> // wifi lib
+#include<DHT.h> //dht lib
+#include<ThingSpeak.h> //thingspeak lib
+
+#define dht_pin D5   //intialize the dht pin connecting to nMCU
+#define dhttype DHT11 //give which type of sensor you are using
+
+DHT dht(dht_pin,dhttype); // give parameter to the dht sensor
+
+long mychannelnum=1532593;
+char myAPI[]="JTPV47LDNWQ86BP7";
+
+WiFiClient user;
+
+const char ssid[]="MI ME";
+const char pass[]="ghghghgh";
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(115200);
+    WiFi.begin(ssid,pass);
+    while(WiFi.status()!= WL_CONNECTED){
+      Serial.print("..");
+      delay(300);
+    }
+    Serial.print("\n nMCU is connected to ");
+    Serial.print( ssid);
+    Serial.println(WiFi.localIP());
+    dht.begin();
+    ThingSpeak.begin(user);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // to read the data 
+  float hum= dht.readHumidity();
+  float temp= dht.readTemperature();
+
+  Serial.print("humidity :");
+  Serial.println( hum);
+  Serial.print("Temperature :");
+  Serial.println( temp);
+  delay(100);
+
+  ThingSpeak.writeField(mychannelnum,1,hum,myAPI);
+  ThingSpeak.writeField(mychannelnum,2,temp,myAPI);
+}
